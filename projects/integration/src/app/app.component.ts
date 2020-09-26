7513991ada747775c366d2c72d0dcd6abc9b8c10
ersimont <8042088+ersimont@users.noreply.github.com>
2020-09-26 14:00:06 -0400

    chore(integration): `ng generate application --prefix=s integration`

---
+++ projects/integration/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 's-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'integration';
+}
