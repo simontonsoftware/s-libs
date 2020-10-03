3bd470c67b5cb23e2e97acfc62ed6dbd5c634b49
ersimont <8042088+ersimont@users.noreply.github.com>
2020-10-03 16:13:51 -0400

    chore(app-state): integration spec

---
+++ projects/integration/src/app/api-tests/app-state.spec.ts
@@ -0,0 +1,34 @@
+import {
+  pushToStoreArray,
+  RootStore,
+  spreadArrayStore$,
+  spreadObjectStore$,
+  Store,
+  UndoManager,
+} from 'app-state';
+
+describe('app-state', () => {
+  it('has RootStore', () => {
+    expect(RootStore).toBeDefined();
+  });
+
+  it('has Store', () => {
+    expect(Store).toBeDefined();
+  });
+
+  it('has UndoManager', () => {
+    expect(UndoManager).toBeDefined();
+  });
+
+  it('has pushToStoreArray', () => {
+    expect(pushToStoreArray).toBeDefined();
+  });
+
+  it('has spreadArrayStore$', () => {
+    expect(spreadArrayStore$).toBeDefined();
+  });
+
+  it('has spreadObjectStore$', () => {
+    expect(spreadObjectStore$).toBeDefined();
+  });
+});
