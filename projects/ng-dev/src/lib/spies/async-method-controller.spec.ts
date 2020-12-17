5b84197fc23d5db57117d40dadaae7958c5d8077
ersimont <8042088+ersimont@users.noreply.github.com>
2020-12-16 19:55:06 -0500

    reorder tests to match the order of methods in the production code

--- projects/ng-dev/src/lib/spies/async-method-controller.spec.ts
+++ projects/ng-dev/src/lib/spies/async-method-controller.spec.ts
@@ -12,88 +12,6 @@ describe('AsyncMethodController', () => {
     });
   });
 
-  describe('.match()', () => {
-    it('finds matching method calls', () => {
-      const controller = new AsyncMethodController(
-        navigator.clipboard,
-        'writeText',
-      );
-      navigator.clipboard.writeText('value 1');
-      navigator.clipboard.writeText('value 2');
-      navigator.clipboard.writeText('value 3');
-
-      const matches = controller.match((call) => call.args[0] !== 'value 2');
-
-      expect(matches.map((match) => match.callInfo.args[0])).toEqual(
-        jasmine.arrayWithExactContents(['value 1', 'value 3']),
-      );
-    });
-
-    it('accepts an array of arguments to match against', () => {
-      const controller = new AsyncMethodController(
-        navigator.clipboard,
-        'writeText',
-      );
-      navigator.clipboard.writeText('value 1');
-      navigator.clipboard.writeText('value 2');
-      navigator.clipboard.writeText('value 1');
-
-      const matches = controller.match(['value 1']);
-
-      expect(matches.map((match) => match.callInfo.args[0])).toEqual(
-        jasmine.arrayWithExactContents(['value 1', 'value 1']),
-      );
-    });
-
-    it('uses deep equality matching for the arguments shorthand', () => {
-      const controller = new AsyncMethodController(window, 'fetch');
-      fetch('a fake url', { method: 'GET' });
-      fetch('a fake url', { method: 'POST' });
-      fetch('a fake url', { method: 'GET' });
-
-      const matches = controller.match(['a fake url', { method: 'GET' }]);
-
-      expect(matches.map((match) => match.callInfo.args[1])).toEqual(
-        jasmine.arrayWithExactContents([{ method: 'GET' }, { method: 'GET' }]),
-      );
-    });
-
-    it('does not remove the matching calls from future matching', () => {
-      const controller = new AsyncMethodController(
-        navigator.clipboard,
-        'readText',
-      );
-      navigator.clipboard.readText();
-      navigator.clipboard.readText();
-
-      controller.match(() => true);
-      const matchesOnSecondTry = controller.match(() => true);
-
-      expect(matchesOnSecondTry.length).toEqual(2);
-    });
-
-    it('returns an empty array when there have been no calls', () => {
-      const controller = new AsyncMethodController(
-        navigator.clipboard,
-        'readText',
-      );
-      const matches = controller.match(() => false);
-      expect(matches).toEqual([]);
-    });
-
-    it('can gracefully handle when no calls match', () => {
-      const controller = new AsyncMethodController(
-        navigator.clipboard,
-        'readText',
-      );
-      navigator.clipboard.readText();
-
-      const matches = controller.match(() => false);
-
-      expect(matches).toEqual([]);
-    });
-  });
-
   describe('.expectOne()', () => {
     it('finds a matching method call', () => {
       const controller = new AsyncMethodController(
@@ -204,6 +122,88 @@ describe('AsyncMethodController', () => {
       expect(() => {
         controller.expectNone(['value 2']);
       }).toThrowMatching((error: Error) => error.message.includes('found 1'));
+    });
+  });
+
+  describe('.match()', () => {
+    it('finds matching method calls', () => {
+      const controller = new AsyncMethodController(
+        navigator.clipboard,
+        'writeText',
+      );
+      navigator.clipboard.writeText('value 1');
+      navigator.clipboard.writeText('value 2');
+      navigator.clipboard.writeText('value 3');
+
+      const matches = controller.match((call) => call.args[0] !== 'value 2');
+
+      expect(matches.map((match) => match.callInfo.args[0])).toEqual(
+        jasmine.arrayWithExactContents(['value 1', 'value 3']),
+      );
+    });
+
+    it('accepts an array of arguments to match against', () => {
+      const controller = new AsyncMethodController(
+        navigator.clipboard,
+        'writeText',
+      );
+      navigator.clipboard.writeText('value 1');
+      navigator.clipboard.writeText('value 2');
+      navigator.clipboard.writeText('value 1');
+
+      const matches = controller.match(['value 1']);
+
+      expect(matches.map((match) => match.callInfo.args[0])).toEqual(
+        jasmine.arrayWithExactContents(['value 1', 'value 1']),
+      );
+    });
+
+    it('uses deep equality matching for the arguments shorthand', () => {
+      const controller = new AsyncMethodController(window, 'fetch');
+      fetch('a fake url', { method: 'GET' });
+      fetch('a fake url', { method: 'POST' });
+      fetch('a fake url', { method: 'GET' });
+
+      const matches = controller.match(['a fake url', { method: 'GET' }]);
+
+      expect(matches.map((match) => match.callInfo.args[1])).toEqual(
+        jasmine.arrayWithExactContents([{ method: 'GET' }, { method: 'GET' }]),
+      );
+    });
+
+    it('does not remove the matching calls from future matching', () => {
+      const controller = new AsyncMethodController(
+        navigator.clipboard,
+        'readText',
+      );
+      navigator.clipboard.readText();
+      navigator.clipboard.readText();
+
+      controller.match(() => true);
+      const matchesOnSecondTry = controller.match(() => true);
+
+      expect(matchesOnSecondTry.length).toEqual(2);
+    });
+
+    it('returns an empty array when there have been no calls', () => {
+      const controller = new AsyncMethodController(
+        navigator.clipboard,
+        'readText',
+      );
+      const matches = controller.match(() => false);
+      expect(matches).toEqual([]);
+    });
+
+    it('can gracefully handle when no calls match', () => {
+      const controller = new AsyncMethodController(
+        navigator.clipboard,
+        'readText',
+      );
+      navigator.clipboard.readText();
+
+      const matches = controller.match(() => false);
+
+      expect(matches).toEqual([]);
     });
   });
 
