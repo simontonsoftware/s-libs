1ec7f5396a138794a83bfa4c5f62ad04c2e70bd6
ersimont <8042088+ersimont@users.noreply.github.com>
2021-10-30 08:08:49 -0400

    chore: remove deprecated `precompileForTests()`
    BREAKING CHANGE: The deprecated `precompileForTests()` was removed from the library. It was only useful for the old View Engine renderer.

--- projects/ng-dev/src/public-api.ts
+++ projects/ng-dev/src/public-api.ts
@@ -7,5 +7,4 @@ export * from './lib/angular-context/index';
 export * from './lib/component-context/index';
 export { logTimers } from './lib/log-timers';
 export { marbleTest } from './lib/marble-test';
-export { precompileForTests } from './lib/precompile-for-tests';
 export { trimLeftoverStyles } from './lib/trim-leftover-styles';
