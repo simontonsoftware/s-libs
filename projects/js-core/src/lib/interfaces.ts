e10e3f6078bba3dff881625d64565c9c7dd74e4a
ersimont <8042088+ersimont@users.noreply.github.com>
2021-11-03 19:34:46 -0400

    chore(js-core): fix an illegal import from micro-dash

--- projects/js-core/src/lib/interfaces.ts
+++ projects/js-core/src/lib/interfaces.ts
@@ -1,5 +1,3 @@
-import { IfIndexType } from '../../../micro-dash/src/lib/interfaces';
-
 export type ArrayIteratee<I, O> = (item: I, index: number) => O;
 export type ObjectIteratee<T, O> = (
   item: T[keyof T],
@@ -10,6 +8,12 @@ export type StringifiedKey<T> = Cast<keyof T, string>;
 type Cast<I, O> = Exclude<I, O> extends never ? I : O;
 
 export type Nil = null | undefined;
+
+type IfIndexType<T, If, Else = never> = string extends T
+  ? If
+  : number extends T
+  ? If
+  : Else;
 
 type IndexKeys<T> = { [K in keyof T]: IfIndexType<K, K> }[keyof T];
 type NonIndexKeys<T> = { [K in keyof T]: IfIndexType<K, never, K> }[keyof T];
