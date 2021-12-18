24787771bbf8454e415e189ae5ee18bca765f420
ersimont <8042088+ersimont@users.noreply.github.com>
2021-12-17 23:29:39 -0500

    feat(ng-mat-core): Add `SlDialogHarness`

--- projects/integration/src/app/api-tests/ng-mat-core.spec.ts
+++ projects/integration/src/app/api-tests/ng-mat-core.spec.ts
@@ -1,6 +1,14 @@
-import { SlDialogModule, SlDialogService } from '@s-libs/ng-mat-core';
+import {
+  SlDialogHarness,
+  SlDialogModule,
+  SlDialogService,
+} from '@s-libs/ng-mat-core';
 
 describe('ng-mat-core', () => {
+  it('has SlDialogHarness', () => {
+    expect(SlDialogHarness).toBeDefined();
+  });
+
   it('has SlDialogModule', () => {
     expect(SlDialogModule).toBeDefined();
   });
--- projects/ng-mat-core/src/lib/dialog/index.ts
+++ projects/ng-mat-core/src/lib/dialog/index.ts
@@ -5,5 +5,6 @@ export {
   DialogData,
   SL_DIALOG_DATA,
 } from './dialog.component';
+export { SlDialogHarness } from './sl-dialog.harness';
 export { SlDialogModule } from './sl-dialog.module';
 export { SlDialogService } from './sl-dialog.service';
