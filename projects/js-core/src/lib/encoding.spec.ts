09b1d55db78ca7e876561ca5c89f03bdbf05a9bf
ersimont <8042088+ersimont@users.noreply.github.com>
2022-08-12 07:54:51 -0400

    feat(js-core): add `Encoding`

---
+++ projects/js-core/src/lib/encoding.spec.ts
@@ -0,0 +1,91 @@
+import { Encoding } from './encoding';
+
+describe('Encoding', () => {
+  let encoding: Encoding;
+  beforeEach(() => {
+    encoding = new Encoding();
+  });
+
+  describe('push & pull string', () => {
+    it('uses the given alphabet', () => {
+      encoding.pushString('abc', 'abcd');
+      expect(encoding.pullAllAsString('ab')).toBe('bba');
+    });
+
+    it('has a nice error when value has a character that is not in the alphabet', () => {
+      expect(() => {
+        encoding.pushString('ab', 'a');
+      }).toThrowError('value contains a character that is not in alphabet: b');
+    });
+  });
+
+  it('can push and pull booleans', () => {
+    encoding.pushBoolean(true);
+    encoding.pushBoolean(false);
+    expect(encoding.pullBoolean()).toBe(false);
+    expect(encoding.pullBoolean()).toBe(true);
+  });
+
+  describe('push and pull options', () => {
+    it('works', () => {
+      encoding.pushOption(4, [2, 4]);
+      encoding.pushOption('hi', ['hi', 'there']);
+      expect(encoding.pullOption(['hi', 'there'])).toBe('hi');
+      expect(encoding.pullOption([2, 4])).toBe(4);
+    });
+
+    it('has a nice error when value is not in options', () => {
+      expect(() => {
+        encoding.pushOption('a', ['b', 'c']);
+      }).toThrowError('value (a) not found in options');
+    });
+  });
+
+  describe('push and pull number', () => {
+    it('uses the given bounds', () => {
+      encoding.pushNumber(8, 5, 11);
+      encoding.pushNumber(-2, -9);
+      encoding.pushNumber(3, 3);
+      expect(encoding.pullNumber(3)).toBe(3);
+      expect(encoding.pullNumber(-9)).toBe(-2);
+      expect(encoding.pullNumber(5, 11)).toBe(8);
+    });
+
+    it('has a nice error message when the value is out of bounds', () => {
+      expect(() => {
+        encoding.pushNumber(-1, 8);
+      }).toThrowError('value (-1) must be at least 0');
+      expect(() => {
+        encoding.pushNumber(5, 4);
+      }).toThrowError('value (5) must be at most 4');
+    });
+  });
+
+  it('has .hasData()', () => {
+    expect(encoding.hasData()).toBe(false);
+    encoding.pushBoolean(true);
+    expect(encoding.hasData()).toBe(true);
+    encoding.pullBoolean();
+    expect(encoding.hasData()).toBe(false);
+  });
+
+  it('works for the example in the docs', () => {
+    const hexAlphabet = '0123456789abcdef';
+    const options = ['disagree', 'neutral', 'agree'];
+
+    // encode some survey answers into a hex string
+    encoding = new Encoding();
+    encoding.pushNumber(5, 1, 5); // encode 5, where it could be between 1-5
+    encoding.pushBoolean(true);
+    encoding.pushOption('agree', options);
+    const hexString = encoding.pullAllAsString(hexAlphabet);
+    expect(hexString).toBe('1d');
+
+    // later decode the string back into the survey answers
+    encoding = new Encoding();
+    encoding.pushString(hexString, hexAlphabet);
+    expect(encoding.pullOption(options)).toBe('agree');
+    expect(encoding.pullBoolean()).toBe(true);
+    expect(encoding.pullNumber(1, 5)).toBe(5);
+  });
+});
---
+++ projects/js-core/src/lib/encoding.ts
@@ -0,0 +1,100 @@
+import { assert } from '@s-libs/js-core';
+import { sort } from './sort';
+
+/**
+ * Convert arbitrary data to and from a compact string representation. Push data into the encoding one field at a time and convert it to a string. Later you can pull it back out in reverse order. The string is as compact as possible because it memorizes only your data, and does not need to capture field names.
+ *
+ * ```ts
+ * const hexAlphabet = '0123456789abcdef';
+ * const options = ['disagree', 'neutral', 'agree'];
+ *
+ * // encode some survey answers into a hex string
+ * encoding = new Encoding();
+ * encoding.pushNumber(5, 1, 5); // encode 5, where it could be between 1-5
+ * encoding.pushBoolean(true);
+ * encoding.pushOption('agree', options);
+ * const hexString = encoding.pullAllAsString(hexAlphabet);
+ * expect(hexString).toBe('1d');
+ *
+ * // later decode the string back into the survey answers
+ * encoding = new Encoding();
+ * encoding.pushString(hexString, hexAlphabet);
+ * expect(encoding.pullOption(options)).toBe('agree');
+ * expect(encoding.pullBoolean()).toBe(true);
+ * expect(encoding.pullNumber(1, 5)).toBe(5);
+ */
+export class Encoding {
+  #asNumber = 0n;
+
+  pushString(value: string, alphabet: string): void {
+    for (const c of value) {
+      const index = alphabet.indexOf(c);
+      assert(
+        index >= 0,
+        `value contains a character that is not in alphabet: ${c}`,
+      );
+      this.#push(index, alphabet.length);
+    }
+  }
+
+  pullAllAsString(alphabet: string): string {
+    let asString = '';
+    while (this.hasData()) {
+      asString = alphabet[this.#pull(alphabet.length)] + asString;
+    }
+    return asString;
+  }
+
+  /**
+   * Push a number between the given bounds (inclusive). The bounds can be specified in either order (`min, max` or `max, min`).
+   */
+  pushNumber(value: number, bound1: number, bound2 = 0): void {
+    const [min, max] = sort([bound1, bound2]);
+    assert(value >= min, `value (${value}) must be at least ${min}`);
+    assert(value <= max, `value (${value}) must be at most ${max}`);
+    this.#push(value - min, max - min + 1);
+  }
+
+  /**
+   * The bounds can be specified in either order (`min, max` or `max, min`).
+   */
+  pullNumber(bound1: number, bound2 = 0): number {
+    const [min, max] = sort([bound1, bound2]);
+    return this.#pull(max - min + 1) + min;
+  }
+
+  pushBoolean(value: boolean): void {
+    this.#push(value ? 1 : 0, 2);
+  }
+
+  pullBoolean(): boolean {
+    return !!this.#pull(2);
+  }
+
+  pushOption<T>(value: T, options: T[]): void {
+    const index = options.indexOf(value);
+    assert(index >= 0, `value (${value}) not found in options`);
+    this.#push(index, options.length);
+  }
+
+  pullOption<T>(options: T[]): T {
+    return options[this.#pull(options.length)];
+  }
+
+  /**
+   * Returns an indication whether data has been push to this encoding but not pulled. **It is possible for this to return `true` even after pushing data.** This happens when the only things that have been pushed are represented internally as 0 (such as the flag `false`, or an option at index `0`).
+   */
+  hasData(): boolean {
+    return this.#asNumber > 0n;
+  }
+
+  #push(value: number, numValues: number): void {
+    this.#asNumber = this.#asNumber * BigInt(numValues) + BigInt(value);
+  }
+
+  #pull(numValues: number): number {
+    const value = this.#asNumber % BigInt(numValues);
+    this.#asNumber /= BigInt(numValues);
+    return Number(value);
+  }
+}
