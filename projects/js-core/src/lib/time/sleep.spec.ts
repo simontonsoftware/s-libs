cd3ac161d1e5e0a98510ea5fa4e336f0d628c5c4
ersimont <8042088+ersimont@users.noreply.github.com>
2022-08-20 11:46:13 -0400

    feat(js-core): `sleep()` now accepts a unit of time

--- projects/js-core/src/lib/time/sleep.spec.ts
+++ projects/js-core/src/lib/time/sleep.spec.ts
@@ -1,5 +1,6 @@
 import { fakeAsync, tick } from '@angular/core/testing';
 import { sleep } from './sleep';
+import { TimeUnit } from './time-utils';
 
 describe('sleep()', () => {
   it('resolves after the given delay', fakeAsync(() => {
@@ -9,6 +10,18 @@ describe('sleep()', () => {
     });
 
     tick(999);
+    expect(resolved).toBe(false);
+    tick(1);
+    expect(resolved).toBe(true);
+  }));
+
+  it('accepts units for the delay', fakeAsync(() => {
+    let resolved = false;
+    sleep(10, TimeUnit.Seconds).then(() => {
+      resolved = true;
+    });
+
+    tick(9_999);
     expect(resolved).toBe(false);
     tick(1);
     expect(resolved).toBe(true);
--- projects/js-core/src/lib/time/sleep.ts
+++ projects/js-core/src/lib/time/sleep.ts
@@ -1,14 +1,20 @@
+import { convertTime } from './time-utils';
+
 /**
- * Returns a promise that resolves after `ms` milliseconds.
+ * Returns a promise that resolves after a specified delay.
+ *
+ * @param unit Can be anything listed in the docs for {@link TimeUnit}. Defaults to milliseconds
  *
  * ```ts
  * // do something
- * await sleep(1000); // wait a second
+ * await sleep(1_000); // wait 1 second
  * // do something else
+ * await sleep(2, 'hours'); // wait 2 hours
+ * // do a final thing
  * ```
  */
-export async function sleep(ms: number): Promise<undefined> {
+export async function sleep(delay: number, unit = 'ms'): Promise<undefined> {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
+    setTimeout(resolve, convertTime(delay, unit, 'ms'));
   });
 }
