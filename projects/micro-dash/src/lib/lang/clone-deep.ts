a7c5eceec067577cc405dcf4cb9e53ab451f0fc1
ersimont <8042088+ersimont@users.noreply.github.com>
2023-06-29 07:56:26 -0400

    feat(micro-dash): reduce the size of `cloneDeep` by using the new [`structuredClone`](https://developer.mozilla.org/en-US/docs/Web/API/structuredClone)

--- projects/micro-dash/src/lib/lang/clone-deep.ts
+++ projects/micro-dash/src/lib/lang/clone-deep.ts
@@ -1,19 +1,10 @@
-import { forOwn } from '../object';
-import { clone } from './clone';
-
 /**
  * This function is like `clone` except that it recursively clones `value`.
  *
  * Contribution to minified bundle size, when it is the only function imported:
  * - Lodash: 12,696 bytes
- * - Micro-dash: 434 bytes
+ * - Micro-dash: 79 bytes
  */
 export function cloneDeep<T>(value: T): T {
-  if (value instanceof Object) {
-    value = clone(value);
-    forOwn(value, (item, key) => {
-      value[key as keyof T] = cloneDeep(item);
-    });
-  }
-  return value;
+  return structuredClone(value);
 }
