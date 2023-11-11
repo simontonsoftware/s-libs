488e45057807e6f8af318f1ab00627a528359893
ersimont <8042088+ersimont@users.noreply.github.com>
2023-11-11 09:37:15 -0500

    chore: fix deploy scripts after upgrades

--- scripts/bump-peer-dependencies.ts
+++ scripts/bump-peer-dependencies.ts
@@ -2,15 +2,20 @@ import { readFileSync, writeFileSync } from 'fs';
 import { format } from 'prettier';
 import { libraries } from './shared';
 
-for (const project of libraries) {
-  const path = `./projects/${project}/package.json`;
-  const packageJson = JSON.parse(readFileSync(path).toString());
-  const version = `^${packageJson.version}`;
-  for (const dependency of libraries) {
-    const key = `@s-libs/${dependency}`;
-    if (packageJson.peerDependencies?.[key]) {
-      packageJson.peerDependencies[key] = version;
+run();
+
+async function run(): Promise<void> {
+  for (const project of libraries) {
+    const path = `./projects/${project}/package.json`;
+    const packageJson = JSON.parse(readFileSync(path).toString());
+    const version = `^${packageJson.version}`;
+    for (const dependency of libraries) {
+      const key = `@s-libs/${dependency}`;
+      if (packageJson.peerDependencies?.[key]) {
+        packageJson.peerDependencies[key] = version;
+      }
     }
+    const text = await format(JSON.stringify(packageJson), { parser: 'json' });
+    writeFileSync(path, text);
   }
-  writeFileSync(path, format(JSON.stringify(packageJson), { parser: 'json' }));
 }
