bd6c8d704ce522ca039a87e49098654487a68054
Eric Simonton <ersimont@gmail.com>
2026-06-10 06:45:36 -0400

    `ng generate library ng-vitest`

---
+++ projects/ng-vitest/src/lib/ng-vitest.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-vitest',
+  imports: [],
+  template: ` <p>ng-vitest works!</p> `,
+  styles: ``,
+})
+export class NgVitest {}
---
+++ projects/ng-vitest/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ng-vitest
+ */
+
+export * from './lib/ng-vitest';
